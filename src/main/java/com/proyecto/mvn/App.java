4a6965c7cd2edc1a821c98a565662b233b2b7c4d
Eduardo Sanchez <esamu13@gmail.com>
2015-10-31 20:50:45 +0100

    1 cambio en des2

--- src/main/java/com/proyecto/mvn/App.java
+++ src/main/java/com/proyecto/mvn/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello SPAIN!" );
+        System.out.println( "Hello des2!" );
     }
 }
